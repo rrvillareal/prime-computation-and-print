3d0a01dd2a259fc4a3a24071bc433699513cc0a9
rrvillareal23 <rrvillareal@ymail.com>
2017-02-02 22:02:16 -0800

    Add files via upload

---
+++ my_prime.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import time
+
+def primes():
+    n = int(input("How many primes should i go up to?"))
+    start_time = time.clock()
+    out = list()
+    X = [True] * (n+1)
+    for p in range(2, n+1):
+        if (X[p]):
+            out.append(p)
+            for i in range(p, n+1, p):
+                X[i] = False
+    print(out)
+    print(round((time.clock() - start_time), 4), "seconds")
